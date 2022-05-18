cb16b1ac848c1ead8057a00cf2b55ad320fcd61f
ngaleuj <ngaleujoedan@gmail.com>
2022-05-18 19:16:49 +0100

    dfdf

--- program.py
+++ program.py
@@ -39,7 +39,7 @@ def login():
                 session.pop('logged', None)
         return render_template('pages/login.html', error=error)
     else:
-        return render_template('pages/login')
+        return render_template('pages/login.html')
 
 @app.route('/logout',methods=['GET'])
 def logout():
